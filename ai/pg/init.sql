17f537db8d066af7aa8a84d160c954b3d94a7bb9
Sandeep Kumar <sandeep.kumar6@com>
2025-09-22 16:03:09 +0530

    chore: Spring AI pg improvements

--- ai/pg/init.sql
+++ ai/pg/init.sql
@@ -1,3 +1,15 @@
+podman volume create postgresml_data
+podman run \
+    -it \
+    -v postgresml_data:/var/lib/postgresql \
+    -p 5433:5432 \
+    -p 8000:8000 \
+    ghcr.io/postgresml/postgresml:2.10.0 \
+    sudo -u postgresml psql -d postgresml
+
+
+
+
 CREATE EXTENSION IF NOT EXISTS vector;
 CREATE EXTENSION IF NOT EXISTS hstore;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -6,7 +18,7 @@ CREATE TABLE IF NOT EXISTS vector_store (
                                             id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     content text,
     metadata json,
-    embedding vector(1536)
+    embedding vector(768)
     );
 
 CREATE INDEX ON vector_store USING HNSW (embedding vector_cosine_ops);
--- ai/src/main/kotlin/com/sk/ai/config/JdbcConfig.kt
+++ ai/src/main/kotlin/com/sk/ai/config/JdbcConfig.kt
@@ -1,14 +1,14 @@
 package com.sk.ai.config
 
 import com.sk.ai.config.properties.VectorStoreProperties
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.datasource.DriverManagerDataSource
-import java.util.Properties
 
 @Configuration(proxyBeanMethods = false)
 @Order(0)
@@ -18,16 +18,24 @@ class JdbcConfig {
     @Bean
     @ConditionalOnProperty(prefix = "vector.pg", name = ["enabled"], havingValue = "true", matchIfMissing = false)
     fun jdbcTemplate(vectorStoreProperties: VectorStoreProperties): JdbcTemplate {
-        val dataSource = DriverManagerDataSource(
-                vectorStoreProperties.pg.url,
-                vectorStoreProperties.pg.username,
-                vectorStoreProperties.pg.password,
-        )
-        dataSource.setDriverClassName("org.postgresql.Driver")
-        dataSource.schema = vectorStoreProperties.pg.schemaName
-        dataSource.connectionProperties = Properties().also {
-            it.setProperty("TimeZone", "Asia/Kolkata")
-        }
+        //System.setProperty("user.timezone", "Asia/Kolkata")
+        val hikariConfig = HikariConfig()
+        hikariConfig.jdbcUrl = vectorStoreProperties.pg.url
+        hikariConfig.username = vectorStoreProperties.pg.username
+        hikariConfig.password = vectorStoreProperties.pg.password
+        hikariConfig.driverClassName = "org.postgresql.Driver"
+        hikariConfig.schema = vectorStoreProperties.pg.schemaName
+        hikariConfig.addDataSourceProperty("cachePrepStmts", "true")
+        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250")
+        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
+        hikariConfig.addDataSourceProperty("useServerPrepStmts", "true")
+        hikariConfig.addDataSourceProperty("TimeZone", "Asia/Kolkata")
+        hikariConfig.maximumPoolSize = 10
+        hikariConfig.minimumIdle = 5
+        hikariConfig.idleTimeout = 30000
+        hikariConfig.connectionTimeout = 20000
+        hikariConfig.maxLifetime = 1800000
+        val dataSource = HikariDataSource(hikariConfig)
         return JdbcTemplate(dataSource)
     }
 }
